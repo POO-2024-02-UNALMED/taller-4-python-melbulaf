139459e140cf9c055964147c8b4458d2b8914b9d
melbulaf <mebulaf@unal.edu.co>
2024-11-26 07:36:10 -0500

    Prueba2

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,9 +1,9 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = 12
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=[]):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=[]):
         self._grupo = grupo
         if asignaturas is None:
             asignaturas = []
